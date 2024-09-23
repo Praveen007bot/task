74a5da2903162f999779fce0628fd67a30b04fb2
praveen <praveenani963@gmail.com>
2024-09-23 19:34:07 +0530

    landing page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,50 +1,172 @@
-import React, { useEffect, useState } from "react";
-import logo from "../assets/image 14.png";
+import React from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { IoPersonSharp } from "react-icons/io5";
 
-const Header = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const { authUser } = useSelector((store) => store.user);
-
-  useEffect(() => {
-    if (authUser) {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
-    }
-  }, [authUser]);
+export default function Navbar(props) {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
-    <header>
-      <nav className="sticky top-0 flex h-20 items-center justify-between border-b bg-background px-8 md:px-6 mx-8">
-        <div>
-          <Link to={"/"}>
-            <img className="w-10" src={logo} alt="logo" />
+    <nav
+      className={
+        (props.transparent
+          ? "top-0 absolute z-50 w-full"
+          : "relative bg-white shadow-lg") +
+        " flex flex-wrap items-center justify-between px-2 py-3 "
+      }
+    >
+      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <Link
+            to={"/"}
+            className={
+              (props.transparent ? "text-white" : "text-gray-800") +
+              " text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+            }
+          >
+            Goal Setter
           </Link>
+          <button
+            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            type="button"
+            onClick={() => setNavbarOpen(!navbarOpen)}
+          >
+            <i
+              className={
+                (props.transparent ? "text-white" : "text-gray-800") +
+                " fas fa-bars"
+              }
+            ></i>
+          </button>
         </div>
-        <div className="flex gap-8 font-medium text-lg">
-          <Link to={"/addgoal"}>Add goal</Link>
-          <Link to={"/focusmode"}>Focus mode</Link>
-          <Link to={"/history"}>History</Link>
-          <Link to={"/about"}>About</Link>
-        </div>
-        {isLoggedIn ? (
-          <Link to={'/profile'}>
-            <div className="font-medium text-xl flex items-center gap-2 bg-gray-300 hover:bg-gray-200 p-2 cursor-pointer">
-              <IoPersonSharp /> Hi {authUser.username}
-            </div>
-          </Link>
-        ) : (
-          <Link to={"/login"}>
-            <button className="bg-black px-4 py-2 text-white font-bold text-lg">
-              Login
-            </button>
-          </Link>
-        )}
-      </nav>
-    </header>
-  );
-};
+        <div
+          className={
+            "lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none" +
+            (navbarOpen ? " block rounded shadow-lg" : " hidden")
+          }
+          id="example-navbar-warning"
+        >
+          <ul className="flex flex-col lg:flex-row list-none mr-auto">
+            <li className="flex items-center">
+              <Link
+                to={"/addgoal"}
+                className={
+                  (props.transparent
+                    ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                    : "text-gray-800 hover:text-gray-600") +
+                  " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                }
+              >
+                <i
+                  className={
+                    (props.transparent
+                      ? "lg:text-gray-300 text-gray-500"
+                      : "text-gray-500") + " text-lg leading-lg mr-2"
+                  }
+                />{" "}
+                Add Goals
+              </Link>
+            </li>
+            <li className="flex items-center">
+              <Link
+                to={"/focusmode"}
+                className={
+                  (props.transparent
+                    ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                    : "text-gray-800 hover:text-gray-600") +
+                  " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                }
+              >
+                <i
+                  className={
+                    (props.transparent
+                      ? "lg:text-gray-300 text-gray-500"
+                      : "text-gray-500") + " text-lg leading-lg mr-2"
+                  }
+                />{" "}
+                Focus Mode
+              </Link>
+            </li>
+            <li className="flex items-center">
+              <Link
+                to={"/history"}
+                className={
+                  (props.transparent
+                    ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                    : "text-gray-800 hover:text-gray-600") +
+                  " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                }
+              >
+                <i
+                  className={
+                    (props.transparent
+                      ? "lg:text-gray-300 text-gray-500"
+                      : "text-gray-500") + " text-lg leading-lg mr-2"
+                  }
+                />{" "}
+                History
+              </Link>
+            </li>
+            <li className="flex items-center">
+              <Link
+                to={"/about"}
+                className={
+                  (props.transparent
+                    ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                    : "text-gray-800 hover:text-gray-600") +
+                  " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                }
+              >
+                <i
+                  className={
+                    (props.transparent
+                      ? "lg:text-gray-300 text-gray-500"
+                      : "text-gray-500") + " text-lg leading-lg mr-2"
+                  }
+                />{" "}
+                About
+              </Link>
+            </li>
+          </ul>
+          <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <li className="flex items-center">
+              <a
+                className={
+                  (props.transparent
+                    ? "lg:text-white lg:hover:text-gray-300 text-gray-800"
+                    : "text-gray-800 hover:text-gray-600") +
+                  " px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                }
+                href="#pablo"
+              >
+                <i
+                  className={
+                    (props.transparent
+                      ? "lg:text-gray-300 text-gray-500"
+                      : "text-gray-500") +
+                    " fa-solid fa-bell text-lg leading-lg "
+                  }
+                />
+                <span className="lg:hidden inline-block ml-2">Star</span>
+              </a>
+            </li>
 
-export default Header;
+            <li className="flex items-center">
+              <Link to={'/login'}>
+                <button
+                  className={
+                    (props.transparent
+                      ? "bg-white text-gray-800 active:bg-gray-100"
+                      : "bg-pink-500 text-white active:bg-pink-600") +
+                    " text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3"
+                  }
+                  type="button"
+                  style={{ transition: "all .15s ease" }}
+                >
+                  login <i className=" ml-2 fas fa-arrow-alt-circle-right"></i>
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
